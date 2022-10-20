9bb4d3505cc11d0410cffef1e69248175293ebbc
JangJaeHack(Jay) <myride.jay@gmail.com>
2022-10-20 15:42:18 +0900

    221020

--- JavaProject/src/practice/PracticeTest.java
+++ JavaProject/src/practice/PracticeTest.java
@@ -1,43 +1,47 @@
 package practice;
 
 public class PracticeTest {
-    static final String SMALL_LETTER_REGEX = "[a-z]";
-    static final String NOT_ALNUM_REGEX = "[^a-z0-9]";
-    static final String KOREAN_REGEX = "[가-힣]";
+       public static void main(String[] args) {
 
-    public static void main(String[] args) {
+                   String str = "Java is Fun.";
+                   String substring1 = str.substring(1, 5); // 문자열을 각 문자로 쪼개어 인덱스를 부여하고 0번째부터 4번째전까지 출력 즉 4글자 출력
+                   String substring2 = str.substring(5); // 시작인덱스를 지정하면 지정한 인덱스 다음의 문자부터 출력을 할수 있음.
+                   String substring3 = str.substring(5, str.length()); // str.length() 생략가능
 
-        String str1 = "hello world world";
-        String str2 = "hello world world";
-        String replace1 = str1.replace("world", "WORLD");
-        System.out.println("replace1 = " + replace1);
-        String replace2 = str2.replace('w', 'W');
-        System.out.println("replace2 = " + replace2);
-        System.out.println();
+                   System.out.println("substring1 = " + substring1);
+                   System.out.println("substring2 = " + substring2);
+                   System.out.println("substring3 = " + substring3);
 
-        String strAll1 = "hello world world";
-        String strAll2 = "hello world world";
-        String strAll3 = "hello world world";
+                   String substring4 = subString(str, 0, 4);
+                   String substring5 = subString(str, 5);
+                   String substring6 = subString(str, 5, str.length()); // str.length() 생략가능
 
-        String replaceAll1 = strAll1.replaceAll("world", "WORLD");
-//        String replaceAll2 = strAll2.replaceAll('w', 'W'); // 문자 불가
-        String replaceAll3 = strAll3.replaceAll(SMALL_LETTER_REGEX, "_"); // 정규 표현식 사용가능
-        System.out.println("replaceAll1 = " + replaceAll1);
-//        System.out.println("replaceAll2 = " + replaceAll2);
-        System.out.println("replaceAll3 = " + replaceAll3);
-        System.out.println();
+                   System.out.println("substring1 = " + substring4);
+                   System.out.println("substring2 = " + substring5);
+                   System.out.println("substring3 = " + substring6);
+               }
 
-        // 입력 데이터 형식 확인 (포맷 확인)
-        String strAll4 = "abasdasd!!012345^~*#";
-        String replaceAllRegEx1 = strAll4.toLowerCase().replaceAll(NOT_ALNUM_REGEX, ""); // 영문자와 숫자 이외 문자 제거
-        System.out.println("replaceAllRegEx1 = " + replaceAllRegEx1);
+               // 객체지향적인 코드는 아님
+               // 객체지향적인 코드는 문자열.substring(...) // 문자열 will be substituted from ... to ...;
+               // 해당 코드 호출은 subString(문자열, ...) // substitute 문자열 from ... to ...;
+               public static String subString(String s, int beginIndex, int endIndex) {
+                   String result = "";
+                   for(int i = beginIndex; i < endIndex; i++) {
+                       result += s.charAt(i);
+                   }
+                   return result;
+               }
 
-        String strAll5 = "가나다라마바사ABCDEFG";
-        String replaceAllRegEx2 = strAll5.replaceAll(KOREAN_REGEX, ""); // 한글 제거
-        System.out.println("replaceAllRegEx2 = " + replaceAllRegEx2);
-        System.out.println();
-    }
+               public static String subString(String s, int beginIndex) {
+                   String result = "";
+                   for(int i = beginIndex; i < s.length(); i++) {
+                       result += s.charAt(i);
+                   }
+                   return result;
+               }
+           }
 
 
-}
+
+
 
