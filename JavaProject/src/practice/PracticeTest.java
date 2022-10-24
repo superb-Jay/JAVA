82a0845e144957026112f3401638e8cce76d1815
JangJaeHack(Jay) <myride.jay@gmail.com>
2022-10-24 21:41:51 +0900

    22.10.24

--- JavaProject/src/practice/PracticeTest.java
+++ JavaProject/src/practice/PracticeTest.java
@@ -5,26 +5,25 @@ import java.util.Arrays;
 public class PracticeTest {
     public static void main(String[] args) {
 
-        String str1 = "010-1234--5678";
-        String[] strSplit1 = str1.split("-");
-        System.out.println("strSplit2 = " + Arrays.toString(strSplit1));
+        // 양변에 있는 공백문자 삭제
+        // 아스키코드에 대해서는 모두 다 작동 잘함
+        String str1 = "        \t\t\n\rhello        \t\t\n\r";
+        System.out.println("Before: \"" + str1 + "\"");
+        System.out.println("After trim: \"" + str1.trim() + "\"");
+        System.out.println("After strip: \"" + str1.strip() + "\"");
+        System.out.println();
 
-        String str2 = "010.1234...5678";
-        String[] strSplit2 = str2.split("[.]+", 2);
-        System.out.println("strSplit1 = " + Arrays.toString(strSplit2));
-
-        String str3 = "22/09/23";
-        String[] strSplit3 = str3.split("/");
-        System.out.println("strSplit3 = " + Arrays.toString(strSplit3));
-        String strJoin = String.join("", strSplit3); // 하나의 문자열로 합치기
-        System.out.println("strJoin = " + strJoin);
-
-        String str4 = "adkasd100200asd1";
-        String[] strSplit4 = str4.split("[a-zA-Z]+");
-        System.out.println("strSplit4 = " + Arrays.toString(strSplit4));
+        // 유니코드에 대해서는 trim() 작동하지 않음
+        // 유니코드에 대해서는 strip()만 작동 (JDK11+)
+        String str2 = '\u2001' + "String    with    space" + '\u2001';
+        System.out.println("Before: \"" + str2 + "\"");
+        System.out.println("After trim: \"" + str2.trim() + "\"");
+        System.out.println("After strip: \"" + str2.strip() + "\"");
     }
-
 }
+
+
+
 
 
 
