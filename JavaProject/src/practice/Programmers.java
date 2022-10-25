d2659579dc95e677ea78f83707c25247b41710a9
JangJaeHack(Jay) <myride.jay@gmail.com>
2022-10-25 14:18:34 +0900

    22.10.25

--- JavaProject/src/practice/Programmers.java
+++ JavaProject/src/practice/Programmers.java
@@ -1,36 +1,22 @@
 package practice;
 
-import java.util.concurrent.CancellationException;
-
-//
-//대문자와 소문자가 섞여있는 문자열 s가 주어집니다. s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True,
-//다르면 False를 return 하는 solution를 완성하세요. 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다.
-// 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
-//예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다.
-//제한사항
-//문자열 s의 길이 : 50 이하의 자연수
-//문자열 s는 알파벳으로만 이루어져 있습니다.
 class Solution {
-    boolean solution(String s) {
-        boolean answer = true;
-        String str = s.toUpperCase();
-        int pCount = 0;
-        int yCount = 0;
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
 
-        for (int i = 0; i < s.length() ; i++) {
-            if(str.charAt(i) == 'P') {
-                pCount++;
-            }else if(str.charAt(i) == 'Y') {
-                yCount++;
+        for (int i = 0; i < priorities.length; i++) {
+            for (int j = 0; j < i ; j++) {
+                if(priorities[i] < priorities[j]) {
+                    int temp = priorities[j];
+                    priorities[i] = priorities[j];
+                    priorities[i] = temp;
+                }
+
             }
-
         }
 
-        if (pCount == yCount) {
-            answer = true;
-        }else{
-            answer = false;
-        }
+
+
 
         return answer;
     }
@@ -40,9 +26,10 @@ public class Programmers {
 
     public static void main(String[] args) {
 
-        String var = "pPoooyY";
+        int[] var = {2,1,3,2};
+        int lo = 2;
 
-        System.out.println(new Solution().solution(var));
+        System.out.println(new Solution().solution(var,lo));
 
     }
 
