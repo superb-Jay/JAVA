da8a0b048c89e2e09c0ba1543e67fd26c00b0189
JangJaeHack(Jay) <myride.jay@gmail.com>
2022-10-26 20:13:09 +0900

    22.10.26

--- JavaProject/src/practice/PracticeTest.java
+++ JavaProject/src/practice/PracticeTest.java
@@ -5,20 +5,19 @@ import java.util.Arrays;
 public class PracticeTest {
     public static void main(String[] args) {
 
-        // 양변에 있는 공백문자 삭제
-        // 아스키코드에 대해서는 모두 다 작동 잘함
-        String str1 = "        \t\t\n\rhello        \t\t\n\r";
-        System.out.println("Before: \"" + str1 + "\"");
-        System.out.println("After trim: \"" + str1.trim() + "\"");
-        System.out.println("After strip: \"" + str1.strip() + "\"");
-        System.out.println();
+        // String 수정 불가 (String Pool에 저장)
+        String str = "this is constant string. Immutable";
+        // str.charAt(0) = 'T'; // 불가능
 
-        // 유니코드에 대해서는 trim() 작동하지 않음
-        // 유니코드에 대해서는 strip()만 작동 (JDK11+)
-        String str2 = '\u2001' + "String    with    space" + '\u2001';
-        System.out.println("Before: \"" + str2 + "\"");
-        System.out.println("After trim: \"" + str2.trim() + "\"");
-        System.out.println("After strip: \"" + str2.strip() + "\"");
+        char[] charArr = str.toCharArray();
+        System.out.println(Arrays.toString(charArr));
+
+        // char[]은 수정 가능 (힙에 저장)
+        // StringBuilder, StringBuffer도 char[]로 구현되어있음
+        charArr[0] = 'T'; // 가능
+        System.out.println(Arrays.toString(charArr));
+        String res = String.valueOf(charArr); // 수정완료. 다시 상수화하기
+        System.out.println(res);
     }
 }
 
