91a85d6d13d4ef2e6be0602bdec34e0cc7d3e77f
JangJaeHack(Jay) <myride.jay@gmail.com>
2022-12-18 00:59:08 +0900

    프로그래머스 이진수더하기

--- JavaProject/src/practice/Programmers.java
+++ JavaProject/src/practice/Programmers.java
@@ -3,28 +3,19 @@ package practice;
 import java.util.Arrays;
 
 class Solution {
-    public int solution(String before, String after) {
-        int answer = 0;
-
-        String[] strArray = before.split("");
-        String[] strArray2 = after.split("");
-        Arrays.sort(strArray);
-        Arrays.sort(strArray2);
-
-        if(new String(Arrays.toString(strArray)).equals(new String(Arrays.toString(strArray2)))) {
-            answer = 1;
-        }
+    public String solution(String bin1, String bin2) {
+        String answer = "";
+        answer = Integer.toBinaryString(Integer.parseInt(bin1,2) + Integer.parseInt(bin2,2));
         return answer;
-
     }
 }
 public class Programmers {
 
     public static void main(String[] args) {
 
-        String before = "allpe";
-        String after = "apple";
-        System.out.println(new Solution().solution(before,after));
+        String bin1 = "10";
+        String bin2 = "11";
+        System.out.println(new Solution().solution(bin1,bin2));
 
     }
 
