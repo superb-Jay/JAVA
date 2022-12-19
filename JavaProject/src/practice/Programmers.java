f0181ae5f7e24d7008d7cd61b2d151c1437ffea8
JangJaeHack(Jay) <myride.jay@gmail.com>
2022-12-20 02:47:34 +0900

    풀이중

--- JavaProject/src/practice/Programmers.java
+++ JavaProject/src/practice/Programmers.java
@@ -2,37 +2,40 @@ package practice;
 
 
 import java.util.Arrays;
+import java.util.Map;
 
 class Solution {
     public int solution(int[][] board) {
         int answer = 0;
         String[] array = new String[board.length];
         String str = "";
+        String str1 = "";
 
-        // 110 = 6 / 100 = 4  / 총 25고 / 빈칸이 16
-
-        for (int i = 0; i < board.length ; i++) {
-            for (int j = 0; j < board[i].length ; j++) {
-                 str += String.valueOf(board[i][j]);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                str += String.valueOf(board[i][j]);
             }
             array[i] = str;
-            str="";
+            str = "";
+            //1,2,4,8,16
         }
 
+
+
+
         System.out.println(Arrays.toString(array));
-
-
-        System.out.println(board.length);
 
 
         return answer;
     }
 }
+
 public class Programmers {
 
     public static void main(String[] args) {
 
         int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}};
+//                int[][] board = {{0, 0, 0, 0,0},{0, 0, 0, 0, 0},{0, 0, 0, 0, 0},{0, 0, 1, 1, 0},{0, 0, 0, 0, 0}};
 
         System.out.println(new Solution().solution(board));
 
