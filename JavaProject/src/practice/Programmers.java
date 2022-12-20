863305b94bf78670a635372de39956ab0dc8cac6
JangJaeHack(Jay) <myride.jay@gmail.com>
2022-12-21 02:52:54 +0900

    Revert "프로그래머스 순서쌍의 개수"
    This reverts commit abcd1e7f3eb26a0efcaeb7b470a51ef21ad2b224.

--- JavaProject/src/practice/Programmers.java
+++ JavaProject/src/practice/Programmers.java
@@ -1,25 +1,39 @@
 package practice;
 
+import java.util.Arrays;
 
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
+    public int solution(String[] spell, String[] dic) {
+        int answer = 2;
+        String checkStr = "";
+        Arrays.sort(spell);
+        for (int i = 0; i < spell.length; i++) {
+            checkStr+=spell[i];
+        }
+//        System.out.println(checkStr);
 
-        for (int i = 1; i <=n; i++) {
-            if(n%i == 0) {
-                answer++;
+        for (int i = 0; i < dic.length ; i++) {
+            char[] chars = dic[i].toCharArray();
+            Arrays.sort(chars);
+            dic[i] = new String(chars);
+
+            if(dic[i].equals(checkStr)) {
+                answer =1;
             }
         }
+//        System.out.println(Arrays.toString(dic));
         return answer;
     }
 }
+
 public class Programmers {
 
     public static void main(String[] args) {
 
-       int n = 100;
+        String[] spell = {"s", "o", "m", "d"};
+        String[] dic = {"moos", "dzx", "smm", "sunmmo", "som"};
 
-        System.out.println(new Solution().solution(n));
+        System.out.println(new Solution().solution(spell, dic));
 
     }
 
