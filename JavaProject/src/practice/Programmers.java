abcd1e7f3eb26a0efcaeb7b470a51ef21ad2b224
JangJaeHack(Jay) <myride.jay@gmail.com>
2022-12-20 18:08:25 +0900

    프로그래머스 순서쌍의 개수

--- JavaProject/src/practice/Programmers.java
+++ JavaProject/src/practice/Programmers.java
@@ -1,39 +1,25 @@
 package practice;
 
-import java.util.Arrays;
 
 class Solution {
-    public int solution(String[] spell, String[] dic) {
-        int answer = 2;
-        String checkStr = "";
-        Arrays.sort(spell);
-        for (int i = 0; i < spell.length; i++) {
-            checkStr+=spell[i];
-        }
-//        System.out.println(checkStr);
+    public int solution(int n) {
+        int answer = 0;
 
-        for (int i = 0; i < dic.length ; i++) {
-            char[] chars = dic[i].toCharArray();
-            Arrays.sort(chars);
-            dic[i] = new String(chars);
-
-            if(dic[i].equals(checkStr)) {
-                answer =1;
+        for (int i = 1; i <=n; i++) {
+            if(n%i == 0) {
+                answer++;
             }
         }
-//        System.out.println(Arrays.toString(dic));
         return answer;
     }
 }
-
 public class Programmers {
 
     public static void main(String[] args) {
 
-        String[] spell = {"s", "o", "m", "d"};
-        String[] dic = {"moos", "dzx", "smm", "sunmmo", "som"};
+       int n = 100;
 
-        System.out.println(new Solution().solution(spell, dic));
+        System.out.println(new Solution().solution(n));
 
     }
 
