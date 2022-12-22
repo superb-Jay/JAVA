04bc178e391cca545aeeab440d348de571dd695d
JangJaeHack(Jay) <myride.jay@gmail.com>
2022-12-22 14:15:47 +0900

    프로그래머스 저주의 숫자 3

--- JavaProject/src/practice/Programmers.java
+++ JavaProject/src/practice/Programmers.java
@@ -1,26 +1,28 @@
 package practice;
 
+import java.util.Arrays;
 
 class Solution {
-    public String solution(int age) {
-        String answer = "";
-        //소문자 아스키는 97부터 ,122
-        String[] strArr = String.valueOf(age).split("");
+    public int solution(int n) {
+        int answer = 0;
+        int curseNum = 3;
 
-        for (int i = 0; i <strArr.length ; i++) {
-            answer += (char)(Integer.parseInt(strArr[i]) + 'a');
+        for (int i = 1; i <=n ; i++) {
+            answer++;
+            while (answer%curseNum == 0 || String.valueOf(answer).contains(String.valueOf(curseNum)) ) {
+                answer++;
+            }
         }
         return answer;
     }
 }
 
-
 public class Programmers {
 
     public static void main(String[] args) {
 
-        int age = 23;
-        System.out.println(new Solution().solution(age));
+        int n = 40;
+        System.out.println(new Solution().solution(n));
 
     }
 
