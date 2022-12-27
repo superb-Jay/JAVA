cda76ab018f88cb030ec475ac618d0b5dbb63df7
JangJaeHack(Jay) <myride.jay@gmail.com>
2022-12-27 17:15:53 +0900

    프로그래머스 피자 나눠 먹기 (2)

--- JavaProject/src/practice/Programmers.java
+++ JavaProject/src/practice/Programmers.java
@@ -1,14 +1,19 @@
 package practice;
 
 
-import java.util.Arrays;
 
 class Solution {
-    public int solution(int[] numbers) {
+    public int solution(int n) {
         int answer = 0;
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
-        answer = Math.max(numbers[numbers.length-1] * numbers[numbers.length-2],numbers[0]*numbers[1]);
+        int slacePizza = 6;
+
+        for (int i = 0; i < slacePizza*n ; i++) {
+            if(slacePizza%n == 0) {
+                answer = slacePizza/6;
+                break;
+            }
+            slacePizza+=6;
+        }
         return answer;
     }
 }
@@ -17,8 +22,8 @@ public class Programmers {
 
     public static void main(String[] args) {
 
-        int[] numbers = {0, -31, 24, 10, 1, 9};
-        System.out.println(new Solution().solution(numbers));
+       int n = 10;
+        System.out.println(new Solution().solution(n));
 
     }
 
