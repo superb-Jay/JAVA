0f8f1c67feba1afbd6f68c4dec0d2f4bda50c7b9
JangJaeHack(Jay) <myride.jay@gmail.com>
2022-12-27 16:56:37 +0900

    프로그래머스 최댓값 만들기 (2)

--- JavaProject/src/practice/Programmers.java
+++ JavaProject/src/practice/Programmers.java
@@ -1,18 +1,14 @@
 package practice;
 
+
 import java.util.Arrays;
 
 class Solution {
-    public int solution(int n) {
+    public int solution(int[] numbers) {
         int answer = 0;
-        int curseNum = 3;
-
-        for (int i = 1; i <=n ; i++) {
-            answer++;
-            while (answer%curseNum == 0 || String.valueOf(answer).contains(String.valueOf(curseNum)) ) {
-                answer++;
-            }
-        }
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+        answer = Math.max(numbers[numbers.length-1] * numbers[numbers.length-2],numbers[0]*numbers[1]);
         return answer;
     }
 }
@@ -21,8 +17,8 @@ public class Programmers {
 
     public static void main(String[] args) {
 
-        int n = 40;
-        System.out.println(new Solution().solution(n));
+        int[] numbers = {0, -31, 24, 10, 1, 9};
+        System.out.println(new Solution().solution(numbers));
 
     }
 
