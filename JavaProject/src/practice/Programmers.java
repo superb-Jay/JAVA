32f882aa7a00624c2aea19ac742e1c81aff06a4e
JangJaeHack(Jay) <myride.jay@gmail.com>
2022-12-28 15:09:49 +0900

    프로그래머스 2차원으로 만들기

--- JavaProject/src/practice/Programmers.java
+++ JavaProject/src/practice/Programmers.java
@@ -1,30 +1,38 @@
 package practice;
 
 class Solution {
-    public String solution(String letter) {
-        String answer = "";
-        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        String[] alpha = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+    public int[][] solution(int[] num_list, int n) {
+        int[][] answer = new int[num_list.length/n][n];
+        int count = 0;
 
-        String[] letterArray = letter.split(" ");
-
-        for (int i = 0; i < letterArray.length ; i++) {
-            for (int j = 0; j < morse.length ; j++) {
-                if (letterArray[i].equals(morse[j])) {
-                    answer += alpha[j];
-                }
+        for (int i = 0; i < num_list.length/n ; i++) {
+            for (int j = 0; j < n ; j++) {
+                answer[i][j] = num_list[count];
+                count++;
             }
         }
+
+//        for (int i = 0; i < answer.length ; i++) {
+//            for (int j = 0; j <answer[i].length ; j++) {
+//                System.out.print(answer[i][j]);
+//            }
+//            System.out.println();
+//        }
+
+
         return answer;
     }
 }
-
 public class Programmers {
 
     public static void main(String[] args) {
 
-       String letter = ".... . .-.. .-.. ---";
-        System.out.println(new Solution().solution(letter));
+        int[] num_list = {1, 2, 3, 4, 5, 6, 7, 8};
+        int n = 2;
+
+
+
+        System.out.println(new Solution().solution(num_list,n));
 
     }
 
