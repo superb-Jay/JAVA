36dcbffb399affb62feef0855f890505ed89b80a
JangJaeHack(Jay) <myride.jay@gmail.com>
2022-12-29 00:19:18 +0900

    프로그래머스 숨어있는 숫자의 덧셈 (2)

--- JavaProject/src/practice/Programmers.java
+++ JavaProject/src/practice/Programmers.java
@@ -1,38 +1,33 @@
 package practice;
 
+
 import java.util.Arrays;
-import java.util.Collections;
 
 class Solution {
-    public int[] solution(int[] emergency) {
-        int[] answer = new int[emergency.length];
+    public int solution(String my_string) {
+        int answer = 0;
 
-        Integer[] array = Arrays.stream(emergency).boxed().toArray(Integer[]::new);
-        Arrays.sort(array, Collections.reverseOrder());
+        String pattern = "[a-zA-z]";
 
-        for (int i = 0; i < emergency.length ; i++) {
-            for (int j = 0; j < array.length ; j++) {
-                if(emergency[i] == array[j]) {
-                    answer[i] = j+1;
-                }
+        String[] strArr = my_string.split(pattern);
+//        System.out.println(Arrays.toString(strArr));
+        for (int i = 0; i < strArr.length; i++) {
+            if (!(strArr[i].equals(""))) {
+                answer += Integer.parseInt(strArr[i]);
             }
         }
-
-        System.out.println(Arrays.toString(array));
-        System.out.println(Arrays.toString(answer));
-
         return answer;
     }
 }
+
 public class Programmers {
 
     public static void main(String[] args) {
 
-        int[] emergency = {3, 76, 24};
+        String my_string = "aAb1B2cC34oOp";
 
 
-
-        System.out.println(new Solution().solution(emergency));
+        System.out.println(new Solution().solution(my_string));
 
     }
 
