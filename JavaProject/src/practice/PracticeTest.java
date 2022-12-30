93a1bd27e436b2aa5bf4d9c3aba7d4e3f490cb5f
JangJaeHack(Jay) <myride.jay@gmail.com>
2022-12-30 20:47:26 +0900

    ArrayList를 활용한 자료구조 Queue 구현

--- JavaProject/src/practice/PracticeTest.java
+++ JavaProject/src/practice/PracticeTest.java
@@ -1,25 +1,33 @@
 package practice;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
-public class PracticeTest {
-    int a, b = 10;
+public class PracticeTest<T> {
 
+    private ArrayList<T> queue = new ArrayList<>();
+
+    public void enqueue(T item) {
+        queue.add(item);
+    }
+
+    public T dequeue() {
+        if(queue.isEmpty()) {
+            return null;
+        }
+        queue.get(0);
+        return queue.remove(0);
+    }
     public static void main(String[] args) {
 
-//        ArrayList<Integer> list = new ArrayList<Integer>();  // 클래스명 뒤에 <타입을> 을 추가하여 사용
-        ArrayList<Integer> list = new ArrayList<>(); // 뒤에는 생략가능
+        PracticeTest<Integer> practiceTest = new PracticeTest();
 
-        list.add(10);     // 원래는 list.add(new Integer(10)); 이 오토 박싱이 되것임. (Integer 클래스 객체라는 말)
-        list.add(20);
-        list.add(30);
-
-//        list.add("30");     // 원래는 list.add(new String("30")); 이 오토박싱 된것임. (String 클래스 객체라는 말)
-
-
-        Integer i = (Integer)list.get(2);
-        System.out.println(i);
+        practiceTest.enqueue(1);
+        practiceTest.enqueue(2);
+        practiceTest.enqueue(3);
+        System.out.println(practiceTest.dequeue());
+        System.out.println(practiceTest.dequeue());
+        System.out.println(practiceTest.dequeue());
+        System.out.println(practiceTest.dequeue());
 
     }
 }
