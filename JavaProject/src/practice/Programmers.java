fb49a7a28c0ed031ee2e2673570ff2a62f1ec044
JangJaeHack(Jay) <myride.jay@gmail.com>
2023-01-02 16:33:55 +0900

    프로그래머스 컨트롤 제트 링크드리스트<스택>활용

--- JavaProject/src/practice/Programmers.java
+++ JavaProject/src/practice/Programmers.java
@@ -1,39 +1,36 @@
 package practice;
 
-
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
 
 class Solution {
     public int solution(String s) {
         int answer = 0;
 
-        ArrayList<String> list = new ArrayList<>(Arrays.asList(s.split(" ")));
+        String[] strArr = s.split(" ");
+        LinkedList<String> list = new LinkedList<>();
 
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).equals("Z")) {
-                list.remove(i);
-                list.set(i-1,"0");
+        for (int i = 0; i < strArr.length ; i++) {
+            if (strArr[i].equals("Z")) {
+                list.pop();
+            }else{
+                list.push(strArr[i]);
             }
         }
-
         System.out.println(list);
 
-        Integer[] answerArr = list.stream().mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new);
-
-        for (Integer integer : answerArr) {
-            answer+=integer;
+        for (String s1 : list) {
+            answer+=Integer.parseInt(s1);
         }
+
         return answer;
     }
-    // 자료구조 스택을 이용해서 풀이를 나중에 다시 해보자.
 }
 public class Programmers {
 
     public static void main(String[] args) {
 
 
-        String s = "10 Z 20 Z";
+        String s = "1 2 Z 3";
 
         System.out.println(new Solution().solution(s));
 
