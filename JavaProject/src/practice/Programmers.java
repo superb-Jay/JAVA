ef38ed2b8c5be241223cbcd9295134592616db78
JangJaeHack(Jay) <myride.jay@gmail.com>
2023-01-02 16:08:14 +0900

    프로그래머스 캐릭터의 좌표- 풀이완료

--- JavaProject/src/practice/Programmers.java
+++ JavaProject/src/practice/Programmers.java
@@ -6,36 +6,34 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(String[] keyinput, int[] board) {
         int[] answer = {0,0};
-        //up y+1, down y-1, left x-1 right x+1
-//      String[] keyinput = {"left", "right", "up", "right", "right"};
+
+
         for (int i = 0; i < keyinput.length ; i++) {
+            if(keyinput[i].equals("left")) {
+                answer[0] -= 1;
+            } else if (keyinput[i].equals("right")) {
+                answer[0] += 1;
+            } else if (keyinput[i].equals("up")) {
+                answer[1] += 1;
+            } else if (keyinput[i].equals("down")) {
+                answer[1] -= 1;
+            }
 
-            switch (keyinput[i]) {
-
-                case "left":
-                    if (Math.abs(answer[0])<board[0]/2) {
-                        answer[0] -= 1;
-                    }
-                    break;
-                case "right":
-                    if(Math.abs(answer[0])<board[0]/2) {
-                        answer[0] += 1;
-                    }
-                    break;
-                case "up":
-                    if(Math.abs(answer[1])<board[1]/2) {
-                        answer[1] += 1;
-                    }
-                    break;
-                case "down":
-                    if(Math.abs(answer[1])<board[1]/2) {
-                        answer[1] -= 1;
-                    }
-                    break;
+            if(answer[0]<0 && answer[0]<-board[0]/2) {
+                answer[0] = -board[0]/2;
+            }
+            if(answer[0]>0 && answer[0]>board[0]/2) {
+                answer[0] = board[0]/2;
+            }
+            if(answer[1]<0 && answer[1]<-board[1]/2) {
+                answer[1] = -board[1]/2;
+            }
+            if(answer[1]>0 && answer[1]>board[1]/2) {
+                answer[1] = board[1]/2;
             }
         }
-        System.out.println(Arrays.toString(answer));
 
+        System.out.println(Arrays.toString(answer));
 
         return answer;
     }
