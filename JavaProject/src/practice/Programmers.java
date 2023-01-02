c418c7ce130f8500a47ae280793b58ea386bed0f
JangJaeHack(Jay) <myride.jay@gmail.com>
2023-01-02 16:28:00 +0900

    프로그래머스 컨트롤 제트

--- JavaProject/src/practice/Programmers.java
+++ JavaProject/src/practice/Programmers.java
@@ -1,53 +1,41 @@
 package practice;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 class Solution {
-    public int[] solution(String[] keyinput, int[] board) {
-        int[] answer = {0,0};
+    public int solution(String s) {
+        int answer = 0;
 
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(s.split(" ")));
 
-        for (int i = 0; i < keyinput.length ; i++) {
-            if(keyinput[i].equals("left")) {
-                answer[0] -= 1;
-            } else if (keyinput[i].equals("right")) {
-                answer[0] += 1;
-            } else if (keyinput[i].equals("up")) {
-                answer[1] += 1;
-            } else if (keyinput[i].equals("down")) {
-                answer[1] -= 1;
-            }
-
-            if(answer[0]<0 && answer[0]<-board[0]/2) {
-                answer[0] = -board[0]/2;
-            }
-            if(answer[0]>0 && answer[0]>board[0]/2) {
-                answer[0] = board[0]/2;
-            }
-            if(answer[1]<0 && answer[1]<-board[1]/2) {
-                answer[1] = -board[1]/2;
-            }
-            if(answer[1]>0 && answer[1]>board[1]/2) {
-                answer[1] = board[1]/2;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals("Z")) {
+                list.remove(i);
+                list.set(i-1,"0");
             }
         }
 
-        System.out.println(Arrays.toString(answer));
+        System.out.println(list);
 
+        Integer[] answerArr = list.stream().mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new);
+
+        for (Integer integer : answerArr) {
+            answer+=integer;
+        }
         return answer;
     }
+    // 자료구조 스택을 이용해서 풀이를 나중에 다시 해보자.
 }
 public class Programmers {
 
     public static void main(String[] args) {
 
 
-        String[] keyinput = {"left", "left", "left", "right"};
-        int[] board = {3,3};
+        String s = "10 Z 20 Z";
 
-
-        System.out.println(new Solution().solution(keyinput,board));
+        System.out.println(new Solution().solution(s));
 
     }
 
