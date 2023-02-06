bdf91d5fd5534c47665afbf375772407a38dd005
heehyohoo <myride.jay@gmail.com>
2023-02-06 20:05:11 +0900

    백준 2753 윤년

--- JavaPractice/src/practice/Main.java
+++ JavaPractice/src/practice/Main.java
@@ -1,38 +1,26 @@
 package practice;
 
-import java.util.Stack;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        Stack<Integer> stack = new Stack<Integer>();
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int K = in.nextInt();
+        String data = br.readLine();
+        int prData = Integer.parseInt(data);
 
-        for(int i = 0; i < K; i++) {
-            int number = in.nextInt();
-
-            if(number == 0) {	// 0이라면 스택에 저장된 top 원소를 지운다.
-                stack.pop();
-            }
-            else {
-                /*
-                 * push() 대신 add()로 대체해도 됨 (똑같이 상단에 원소를 추가하는 메소드다.)
-                 * ex) stack.add(number);
-                 */
-                stack.push(number);
-            }
-        }
-        int sum = 0;
-
-        for(int o : stack) {
-            sum += o;
+        if((prData%4 == 0 && prData%100 != 0)||prData%400 == 0) {
+            sb.append(1);
+        }else{
+            sb.append(0);
         }
 
-        System.out.println(sum);
+        System.out.println(sb);
     }
-
 }
