bfc7a8c3533f3a07af832f9566a61e1e5262ce70
Jay <myride.jay@gmail.com>
2023-02-06 21:10:42 +0900

    백준 2884 알람 시계

--- JavaPractice/src/practice/Main.java
+++ JavaPractice/src/practice/Main.java
@@ -4,21 +4,31 @@ package practice;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int prData = Integer.parseInt(br.readLine());
+//        int data = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        if((prData%4 == 0 && prData%100 != 0)||prData%400 == 0) {
-            sb.append(1);
-        }else{
-            sb.append(0);
+        int hNumber = Integer.parseInt(st.nextToken());
+        int mNumber = Integer.parseInt(st.nextToken());
+
+        if(mNumber >= 45) {
+            sb.append(hNumber + " ");
+            sb.append(mNumber - 45);
+        }else if (hNumber > 0 && mNumber < 45) {
+            sb.append(hNumber - 1+ " ");
+            sb.append(mNumber + 15);
+        }else {
+            hNumber = 23;
+            sb.append(hNumber+ " ");
+            sb.append(mNumber+15);
         }
-
         System.out.println(sb);
     }
 }
