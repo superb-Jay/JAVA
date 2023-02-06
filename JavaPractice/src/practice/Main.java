c15d1f5c9c599d9cb2251e408558f3d2493eb97e
Jay <myride.jay@gmail.com>
2023-02-07 01:08:36 +0900

    백준 20053 최소, 최대 2

--- JavaPractice/src/practice/Main.java
+++ JavaPractice/src/practice/Main.java
@@ -4,31 +4,32 @@ package practice;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        int data = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
+//        StringBuilder sb = new StringBuilder();
 
-        int hNumber = Integer.parseInt(st.nextToken());
-        int mNumber = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            int[] arr = new int[N];
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-        if(mNumber >= 45) {
-            sb.append(hNumber + " ");
-            sb.append(mNumber - 45);
-        }else if (hNumber > 0 && mNumber < 45) {
-            sb.append(hNumber - 1+ " ");
-            sb.append(mNumber + 15);
-        }else {
-            hNumber = 23;
-            sb.append(hNumber+ " ");
-            sb.append(mNumber+15);
+            for (int j = 0; j < N ; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+
+//            sb.append(arr.get(0) + " ");
+//            sb.append(arr.get(arr.size() - 1));
+//            System.out.println(sb);
+//            sb = new StringBuilder();
+            System.out.println(arr[0] +" "+ arr[N-1]);
         }
-        System.out.println(sb);
     }
 }
